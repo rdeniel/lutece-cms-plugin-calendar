c0f658d4b51ffd82e72d405d7119616b2796f9a4
RPU <RPU@bab10101-e421-0410-a517-8ce0973de3ef>
2014-03-12 12:49:36 +0000

    CALENDAR-123 Update Lucène version
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/cms/plugin-calendar@64778 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/calendar/service/search/CalendarIndexer.java
+++ src/java/fr/paris/lutece/plugins/calendar/service/search/CalendarIndexer.java
@@ -50,21 +50,30 @@ import fr.paris.lutece.portal.service.plugin.PluginService;
 import fr.paris.lutece.portal.service.search.IndexationService;
 import fr.paris.lutece.portal.service.search.SearchIndexer;
 import fr.paris.lutece.portal.service.search.SearchItem;
+import fr.paris.lutece.portal.service.util.AppException;
 import fr.paris.lutece.portal.service.util.AppPathService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
 import fr.paris.lutece.util.url.UrlItem;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.lucene.demo.html.HTMLParser;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.FieldType;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.html.HtmlParser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.ContentHandler;
+import org.xml.sax.SAXException;
 
 
 /**
@@ -208,9 +217,14 @@ public class CalendarIndexer implements SearchIndexer
         // make a new, empty document
         org.apache.lucene.document.Document doc = new org.apache.lucene.document.Document( );
 
+        FieldType ft = new FieldType( StringField.TYPE_STORED );
+        ft.setOmitNorms( false );
+
+        FieldType ftNotStored = new FieldType( StringField.TYPE_NOT_STORED );
+        ft.setOmitNorms( false );
+
         //add the id of the calendar
-        doc.add( new Field( Constants.FIELD_CALENDAR_ID, strAgenda + "_" + Constants.CALENDAR_SHORT_NAME,
-                Field.Store.NO, Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( Constants.FIELD_CALENDAR_ID, strAgenda + "_" + Constants.CALENDAR_SHORT_NAME, ftNotStored ) );
 
         //add the category of the event
         Collection<Category> arrayCategories = occurrence.getListCategories( );
@@ -226,43 +240,47 @@ public class CalendarIndexer implements SearchIndexer
             }
         }
 
-        doc.add( new Field( Constants.FIELD_CATEGORY, strCategories, Field.Store.NO, Field.Index.ANALYZED ) );
+        doc.add( new Field( Constants.FIELD_CATEGORY, strCategories, TextField.TYPE_NOT_STORED ) );
 
-        doc.add( new Field( SearchItem.FIELD_ROLE, strRoleKey, Field.Store.YES, Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_ROLE, strRoleKey, ft ) );
 
         // Add the url as a field named "url".  Use an UnIndexed field, so
         // that the url is just stored with the question/answer, but is not searchable.
-        doc.add( new Field( SearchItem.FIELD_URL, strUrl, Field.Store.YES, Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_URL, strUrl, ft ) );
 
         // Add the uid as a field, so that index can be incrementally maintained.
         // This field is not stored with question/answer, it is indexed, but it is not
         // tokenized prior to indexing.
         String strIdEvent = String.valueOf( occurrence.getId( ) );
-        doc.add( new Field( SearchItem.FIELD_UID, strIdEvent + "_" + Constants.CALENDAR_SHORT_NAME, Field.Store.YES,
-                Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_UID, strIdEvent + "_" + Constants.CALENDAR_SHORT_NAME, ft ) );
 
         // Add the last modified date of the file a field named "modified".
         // Use a field that is indexed (i.e. searchable), but don't tokenize
         // the field into words.
         String strDate = Utils.getDate( occurrence.getDate( ) );
-        doc.add( new Field( SearchItem.FIELD_DATE, strDate, Field.Store.YES, Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_DATE, strDate, ft ) );
 
         String strContentToIndex = getContentToIndex( occurrence );
-        StringReader readerPage = new StringReader( strContentToIndex );
-        HTMLParser parser = new HTMLParser( readerPage );
+        ContentHandler handler = new BodyContentHandler( );
+        Metadata metadata = new Metadata( );
 
-        //the content of the event descriptionr is recovered in the parser because this one
-        //had replaced the encoded caracters (as &eacute;) by the corresponding special caracter (as ?)
-        Reader reader = parser.getReader( );
-        int c;
-        StringBuffer sb = new StringBuffer( );
-
-        while ( ( c = reader.read( ) ) != -1 )
+        try
         {
-            sb.append( String.valueOf( (char) c ) );
+            new HtmlParser( ).parse( new ByteArrayInputStream( strContentToIndex.getBytes( ) ), handler, metadata,
+                    new ParseContext( ) );
+        }
+        catch ( SAXException e )
+        {
+            throw new AppException( "Error during page parsing." );
+        }
+        catch ( TikaException e )
+        {
+            throw new AppException( "Error during page parsing." );
         }
 
-        reader.close( );
+        //the content of the article is recovered in the parser because this one
+        //had replaced the encoded caracters (as &eacute;) by the corresponding special caracter (as ?)
+        StringBuilder sb = new StringBuilder( handler.toString( ) );
 
         // Add the description as a summary field, so that index can be incrementally maintained.
         // This field is stored, but it is not indexed
@@ -282,18 +300,17 @@ public class CalendarIndexer implements SearchIndexer
         {
         }
 
-        doc.add( new Field( SearchItem.FIELD_SUMMARY, strDescription, Field.Store.YES, Field.Index.ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_SUMMARY, strDescription, TextField.TYPE_STORED ) );
 
         // Add the tag-stripped contents as a Reader-valued Text field so it will
         // get tokenized and indexed.
-        doc.add( new Field( SearchItem.FIELD_CONTENTS, sb.toString( ), Field.Store.NO, Field.Index.ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_CONTENTS, sb.toString( ), TextField.TYPE_NOT_STORED ) );
 
         // Add the subject name as a separate Text field, so that it can be searched
         // separately.
-        doc.add( new Field( SearchItem.FIELD_TITLE, occurrence.getTitle( ), Field.Store.YES, Field.Index.ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_TITLE, occurrence.getTitle( ), TextField.TYPE_STORED ) );
 
-        doc.add( new Field( SearchItem.FIELD_TYPE, CalendarPlugin.PLUGIN_NAME, Field.Store.YES,
-                Field.Index.NOT_ANALYZED ) );
+        doc.add( new Field( SearchItem.FIELD_TYPE, CalendarPlugin.PLUGIN_NAME, ft ) );
 
         // return the document
         return doc;
--- src/java/fr/paris/lutece/plugins/calendar/service/search/CalendarLuceneSearchEngine.java
+++ src/java/fr/paris/lutece/plugins/calendar/service/search/CalendarLuceneSearchEngine.java
@@ -41,7 +41,6 @@ import fr.paris.lutece.portal.service.search.SearchResult;
 import fr.paris.lutece.portal.service.util.AppLogService;
 import fr.paris.lutece.portal.service.util.AppPropertiesService;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -52,18 +51,20 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.lucene.document.DateTools;
 import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.queryParser.MultiFieldQueryParser;
+import org.apache.lucene.queryparser.classic.MultiFieldQueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.Filter;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.Searcher;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TermRangeQuery;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.util.BytesRef;
 
 
 /**
@@ -97,14 +98,15 @@ public class CalendarLuceneSearchEngine implements CalendarSearchEngine
         {
             return new ArrayList<SearchResult>( );
         }
-        Searcher searcher = null;
+        IndexSearcher searcher = null;
 
         //Filter filterRole = getFilterRoles( request );
         Filter filterRole = null;
 
         try
         {
-            searcher = new IndexSearcher( IndexationService.getDirectoryIndex( ), true );
+            IndexReader ir = DirectoryReader.open( IndexationService.getDirectoryIndex( ) );
+            searcher = new IndexSearcher( ir );
 
             Collection<String> queriesForSearchInContent = new ArrayList<String>( );
             Collection<String> queriesForSearchInTitle = new ArrayList<String>( );
@@ -191,8 +193,8 @@ public class CalendarLuceneSearchEngine implements CalendarSearchEngine
             //Dates
             if ( ( dateBegin != null ) && ( dateEnd != null ) )
             {
-                String strDateBegin = Utils.getDate( dateBegin );
-                String strDateEnd = Utils.getDate( dateEnd );
+                BytesRef strDateBegin = new BytesRef( Utils.getDate( dateBegin ) );
+                BytesRef strDateEnd = new BytesRef( Utils.getDate( dateEnd ) );
                 Query queryDate = new TermRangeQuery( SearchItem.FIELD_DATE, strDateBegin, strDateEnd, true, true );
                 queriesForSearchInContent.add( queryDate.toString( ) );
                 queriesForSearchInTitle.add( queryDate.toString( ) );
@@ -255,20 +257,6 @@ public class CalendarLuceneSearchEngine implements CalendarSearchEngine
         catch ( Exception e )
         {
             AppLogService.error( e.getMessage( ), e );
-        }
-        finally
-        {
-            try
-            {
-                if ( searcher != null )
-                {
-                    searcher.close( );
-                }
-            }
-            catch ( IOException ioe )
-            {
-                AppLogService.error( ioe.getMessage( ), ioe );
-            }
         }
 
         return convertList( listResults );
